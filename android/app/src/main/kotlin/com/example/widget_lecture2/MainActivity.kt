3c6e365e639ce9bb61713c83274d74dc6da66ad8
fitriayana <fitriayana782@gmail.com>
2024-05-26 18:13:21 +0800

    first init github

---
+++ android/app/src/main/kotlin/com/example/widget_lecture2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widget_lecture2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            LeafBorderText(
+              text: 'Gambar 1',
+              imgCode: '1',
+            ),
+            LeafBorderText(
+              text: 'Gambar 2',
+              imgCode: '2',
+            ),
+            LeafBorderText(
+              imgCode: '0',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LeafBorderText extends StatelessWidget {
+  final String imgCode;
+  final String text;
+  const LeafBorderText(
+      {super.key, this.text = 'halo dunia', this.imgCode = '1'});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 219, 143, 72),
+          border: Border.all(),
+          borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      child: Padding(
+        padding: EdgeInsets.all(9),
+        child: Row(
+          children: [
+            Image(
+                image: NetworkImage(
+                    'https://picsum.photos/200/300/?random=$imgCode')),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              text,
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 27, 124, 153),
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
